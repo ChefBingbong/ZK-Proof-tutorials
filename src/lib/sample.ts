04839e9ef6cae02a9f28cfdce6ba7ad984d05fb1
BingBong <bingbong@pancakeswap.com>
2024-06-28 13:00:59 +0100

    add affine point types

--- src/lib/sample.ts
+++ src/lib/sample.ts
@@ -1,9 +1,11 @@
 import { secp256k1 } from "@noble/curves/secp256k1";
 import { randBetween } from "bigint-crypto-utils";
 
+import type { AffinePoint } from "./types/common.types.js";
+
 export const N = secp256k1.CURVE.n;
 
-export const sampleScalarPointPair = (): [bigint, any] => {
+export const sampleScalarPointPair = (): [bigint, AffinePoint] => {
       const scalar = randBetween(N - 1n);
       const point = secp256k1.ProjectivePoint.BASE.multiply(scalar);
       return [scalar, point.toAffine()];
