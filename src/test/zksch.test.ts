f9a750fcffe9580105c7fb8eef0a5fc85f6d3403
BingBong <bingbong@pancakeswap.com>
2024-06-28 17:03:28 +0100

    chore use sha256 instead of custom hasher to simplify tutorial

--- src/test/zksch.test.ts
+++ src/test/zksch.test.ts
@@ -8,25 +8,22 @@ import {
       zkSchVerifyResponse,
 } from "../zksch.js";
 import { sampleScalar, sampleScalarPointPair } from "../lib/utils.js";
-import { Hasher } from "../lib/Hasher.js";
 import { secp256k1 } from "@noble/curves/secp256k1";
 
 describe("zk/sch", () => {
-      const hasher = Hasher.create().update("test");
-
       test("pass", () => {
             const a = zkSchCreateRandomness();
             const [x, X] = sampleScalarPointPair();
 
-            const proof = zkSchProve(a, hasher.clone(), X, x);
+            const proof = zkSchProve(a, X, x);
             if (!proof) {
                   throw new Error("proof should not be null");
             }
             assert(
-                  zkSchVerifyResponse(proof, hasher.clone(), X, a.commitment),
+                  zkSchVerifyResponse(proof, X, a.commitment),
                   "failed to verify response"
             );
-            assert(zkSchVerifyResponse(proof, hasher.clone(), X, a.commitment));
+            assert(zkSchVerifyResponse(proof, X, a.commitment));
       });
 
       test("fail", () => {
@@ -35,9 +32,9 @@ describe("zk/sch", () => {
                   sampleScalar(),
                   secp256k1.ProjectivePoint.ZERO.toAffine(),
             ];
-            const proof = zkSchProve(a, hasher.clone(), X, x);
+            const proof = zkSchProve(a, X, x);
             assert.equal(
-                  zkSchVerifyResponse(proof, hasher.clone(), X, a.commitment),
+                  zkSchVerifyResponse(proof, X, a.commitment),
                   false,
                   "proof should not accept identity point"
             );
@@ -49,12 +46,12 @@ describe("zk/sch", () => {
             const publicKey =
                   secp256k1.ProjectivePoint.BASE.multiply(secret).toAffine();
 
-            const proof = zkSchCreateProof(hasher.clone(), publicKey, secret, gen);
+            const proof = zkSchCreateProof(publicKey, secret, gen);
             if (!proof) {
                   throw new Error("proof should not be null");
             }
             assert(
-                  zkSchVerifyProof(proof, hasher.clone(), publicKey, gen),
+                  zkSchVerifyProof(proof, publicKey, gen),
                   "failed to verify proof created by createProof"
             );
       });
