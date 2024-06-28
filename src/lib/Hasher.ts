70753192795691267026f98a0815b946ab1b1fd0
BingBong <bingbong@pancakeswap.com>
2024-06-28 13:18:34 +0100

    add sch test and add update methods to hasher

--- src/lib/Hasher.ts
+++ src/lib/Hasher.ts
@@ -31,9 +31,16 @@ const pointToHashable = (data: AffinePoint | ProjectivePoint): Hashable => {
 
 export class Hasher {
       private hash: ReturnType<typeof blake3.create>;
+      private used = false;
 
       constructor(hash?: ReturnType<typeof blake3.create>) {
             this.hash = hash ?? blake3.create({});
+      }
+
+      private checkUsed() {
+            if (this.used) {
+                  throw new Error("Hasher already used");
+            }
       }
 
       public static create(): Hasher {
@@ -42,5 +49,65 @@ export class Hasher {
 
       public clone(): Hasher {
             return new Hasher(this.hash.clone());
+      }
+
+      public update(data: Ingestable): Hasher {
+            this.checkUsed();
+            let buf: Array<IngestableBasic> = [];
+            if (
+                  data instanceof Uint8Array ||
+                  typeof data === "string" ||
+                  typeof data === "bigint"
+            ) {
+                  buf.push(data);
+            } else if (typeof (data as Hashable).hashable === "function") {
+                  buf = buf.concat((data as Hashable).hashable());
+            } else if (isPoint(data)) {
+                  buf = buf.concat(
+                        pointToHashable(
+                              data as ProjectivePoint | AffinePoint
+                        ).hashable()
+                  );
+            } else {
+                  throw new Error(`Unsupported data type: ${data}`);
+            }
+            for (let i = 0; i < buf.length; i += 1) {
+                  this.updateBasic(buf[i]);
+            }
+            return this;
+      }
+
+      private updateBasic(data: IngestableBasic): Hasher {
+            this.checkUsed();
+            let buf: Input;
+            if (data instanceof Uint8Array) {
+                  buf = data;
+            } else if (typeof data === "string") {
+                  buf = data;
+            } else if (typeof data === "bigint") {
+                  let hex = data.toString(16);
+                  if (hex.length % 2 === 1) {
+                        hex = `0${hex}`;
+                  }
+                  buf = hexToBytes(hex);
+            } else {
+                  throw new Error("Unsupported data type", data);
+            }
+            this.hash.update(buf);
+            return this;
+      }
+
+      public updateMulti(data: Array<Ingestable>): Hasher {
+            this.checkUsed();
+            for (let i = 0; i < data.length; i += 1) {
+                  this.update(data[i]);
+            }
+            return this;
+      }
+
+      public digestBigint(): bigint {
+            this.checkUsed();
+            this.used = true;
+            return bytesToNumberBE(this.hash.digest());
       }
 }
---
+++ src/test/zksch.test.ts
@@ -0,0 +1,39 @@
+import { describe, test } from "node:test";
+import assert from "node:assert/strict";
+import { zkSchCreateRandomness, zkSchProve, zkSchVerifyResponse } from "../zksch.js";
+import { sampleScalar, sampleScalarPointPair } from "../lib/sample.js";
+import { Hasher } from "../lib/Hasher.js";
+import { secp256k1 } from "@noble/curves/secp256k1";
+
+describe("zk/sch", () => {
+      const hasher = Hasher.create().update("test");
+
+      test("pass", () => {
+            const a = zkSchCreateRandomness();
+            const [x, X] = sampleScalarPointPair();
+
+            const proof = zkSchProve(a, hasher.clone(), X, x);
+            if (!proof) {
+                  throw new Error("proof should not be null");
+            }
+            assert(
+                  zkSchVerifyResponse(proof, hasher.clone(), X, a.commitment),
+                  "failed to verify response"
+            );
+            assert(zkSchVerifyResponse(proof, hasher.clone(), X, a.commitment));
+      });
+
+      test("fail", () => {
+            const a = zkSchCreateRandomness();
+            const [x, X] = [
+                  sampleScalar(),
+                  secp256k1.ProjectivePoint.ZERO.toAffine(),
+            ];
+            const proof = zkSchProve(a, hasher.clone(), X, x);
+            assert.equal(
+                  zkSchVerifyResponse(proof, hasher.clone(), X, a.commitment),
+                  false,
+                  "proof should not accept identity point"
+            );
+      });
+});
